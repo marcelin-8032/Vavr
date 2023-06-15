4225f31e563b66d9772a553e4cd2581794b07e71
Mohsen Dahesh <mdehesh@gmail.com>
2023-06-15 21:43:41 +0200

    Initial commit

---
+++ src/test/java/functions/either/EitherTest.java
@@ -0,0 +1,22 @@
+package functions.either;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class EitherTest {
+
+
+    @Test
+    void testEitherTest1(){
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/test/java/functions/option/OptionTest.java
@@ -0,0 +1,19 @@
+package functions.option;
+
+import io.vavr.control.Option;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+@Slf4j
+public class OptionTest {
+
+
+    @Test
+    void optionTest(){
+        Optional<String> myFirstValue= Optional.of("someValues");
+
+        Option<String> a;
+    }
+}
