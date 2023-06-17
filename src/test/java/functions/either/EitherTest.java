1d45d735c084af634938076af14056d247b5e32b
Mohsen Dahesh <mdehesh@gmail.com>
2023-06-17 22:44:48 +0200

    add either

--- src/test/java/functions/either/EitherTest.java
+++ src/test/java/functions/either/EitherTest.java
@@ -1,21 +1,36 @@
 package functions.either;
 
+import io.vavr.control.Either;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class EitherTest {
+class EitherTest {
 
 
     @Test
-    void testEitherTest1(){
+    void testEitherTest1() {
+        Either<String, Integer> either = calculate(-2);
+        log.info("Result: " + either);
 
 
+        if (either.isLeft()) {
+            String left = either.getLeft();
+            log.info("left value" + left);
+        }
 
 
+        if (either.isRight()){
+            Integer right = either.get();
+            log.info("right value" + right);
+        }
 
+    }
 
-
+    private Either<String, Integer> calculate(int i) {
+        if (i <= 0)
+            return Either.left("The argument value should be more than zero. \nThe argument you passed was" + i);
+        return Either.right(i * 10);
     }
 
 
